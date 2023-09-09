0b7220fcb88821ba43e6115509b2e0d0a6db2d8b
Charlie Angela Mehlenbeck <charlie_inventor2003@yahoo.com>
2023-09-08 22:33:31 -0400

    created table that auto populates question answer results from a llm for each job description, without duplicate requests

--- JobDatabase.py
+++ JobDatabase.py
@@ -1,9 +1,21 @@
-from sqlalchemy import create_engine, Column, Integer, String, DateTime
+from sqlalchemy import create_engine, Column, Integer, String, DateTime, Text, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
+from typing import List
+import regex as re
+
+from datetime import datetime, timedelta
+
 Base = declarative_base()
 
+class Question:
+	def __init__(self, column_name:str, short_description:str, long_description:str, answer_format:str):
+		self.column_name = column_name
+		self.short_description = short_description
+		self.long_description = long_description
+		self.answer_format = answer_format
+		
 class Job(Base):
 	__tablename__ = 'jobs'
 
@@ -20,7 +32,43 @@ class Job(Base):
 	full_description_html_path = Column(String)
 	search_term = Column(String)
 	search_location = Column(String)
+	
+	@property
+	def actual_date_posted(self):
+		# Parse date_time_loaded to datetime object
+		date_time_loaded = datetime.strptime(self.date_time_loaded, "%Y-%m-%d %H-%M-%S.%f")
+		
+		# Regular expression patterns for different formats
+		patterns = [
+			("Active (\d+) days ago", timedelta(days=-1)),
+			("Posted (\d+)\+? days ago", timedelta(days=-1)),
+			("Just posted", timedelta(days=0)),
+			("Today", timedelta(days=0))
+		]
+		
+		for pattern, delta in patterns:
+			match = re.search(pattern, self.date_posted)
+			if match:
+				if delta.days == -1:
+					days_ago = int(match.group(1))
+					return date_time_loaded + timedelta(days=-days_ago)
+				else:
+					return date_time_loaded + delta
+		return date_time_loaded
+	
+class JobSearchFilter(Base):
+	__tablename__ = 'job_search_filter'
 
+	id = Column(Integer, primary_key=True)
+	job_link = Column(String, ForeignKey('jobs.link'))
+	conversation_hash = Column(String)
+	# Columns will be added dynamically
+	
+	@classmethod
+	def add_dynamic_columns(cls, questions: List[Question]):
+		for q in questions:
+			setattr(cls, q.column_name, Column(Text))
+	
 class JobDatabase:
 	def __init__(self, db_url="sqlite:///Job_Data/jobs.db"):
 		self.engine = create_engine(db_url)
@@ -40,8 +88,38 @@ class JobDatabase:
 		Session = sessionmaker(bind=self.engine)
 		session = Session()
 
-		return session.query(Job).all()
+		jobs = session.query(Job).all()
+		return sorted(jobs, key=lambda job: job.actual_date_posted, reverse=True)
+	
+	def get_jobs_with_conversations(self):
+		Session = sessionmaker(bind=self.engine)
+		session = Session()
 
+		return session.query(JobSearchFilter).all()
+	
+	def insert_responses(self, conversation_hash, job_link, questions, llm_response):
+		Session = sessionmaker(bind=self.engine)
+		session = Session()
+		new_entry = JobSearchFilter()
+		new_entry.conversation_hash = conversation_hash
+		new_entry.job_link = job_link
+
+		for i in range(len(questions) - 1):
+			pattern = re.compile(f"{questions[i].short_description}: (.*?)\n{questions[i+1].short_description}:", re.DOTALL)
+			match = pattern.search(llm_response)
+			if match:
+				setattr(new_entry, questions[i].column_name, match.group(1).strip())
+
+		# For the last question
+		pattern = re.compile(f"{questions[-1].short_description}: (.*)", re.DOTALL)
+		match = pattern.search(llm_response)
+		if match:
+			setattr(new_entry, questions[-1].column_name, match.group(1).strip())
+
+		session.add(new_entry)
+		session.commit()
+		return new_entry
+		
 if __name__ == "__main__":
 	job_db = JobDatabase()
 	
--- JobDescriptionClassifier.py
+++ JobDescriptionClassifier.py
@@ -3,47 +3,50 @@ from AbstractAI.LLMs.OpenAI_LLM import *
 from AbstractAI.LLMs.StableBeluga2 import *
 from AbstractAI.DB.Database import *
 
+from JobDatabase import JobDatabase, Job, JobSearchFilter, Question
+
 conv_db = Database("sqlite:///job_description_classifications.sql")
 
 model = None
 local = False
 if local:
-	model = StableBeluga2()
+	model = StableBeluga2("stabilityai/StableBeluga2")
 else:
-	model = OpenAI_LLM(model_name="gpt-3.5-turbo-16k")
+	model = OpenAI_LLM(model_name="gpt-3.5-turbo")
 	
 bot = ChatBot(model, conv_db)
 
-system_message = Message(
-		r"""You are an automated job description quality checker. It is very important that you respond in a certain format so that your response can be parsed automatically to sort job descriptions programmatically. You will be given a copy of the user's resume and a job description. You must then look in the job description for several key things and rate the job based on the user's experience. Look for the following things:
 
-1. If any form of education preference is mentioned in the job description.
-2. If there is a hard requirement for a degree mentioned in the job description.3. If the job mentions any skills not listed in the resume.
-4. On a scale of 1-10, how well does the user's experience match the job description?
-5. On a scale of 1-10, how much of the users experience will be effectively utilized in this job?
-6. On a scale of 1-10, how much the user might enjoy this job.
+
+
+questions = [
+	Question("Education", "Education Preference", "Is there any form of preference for education is mentioned in the job description. For example if any of these things are mentioned, masters, bachelors. doctorate, ms, bs, phd, degree, education, etc. Even if they say 'or equivalent experience', then 'education' is mentioned.", "Yes/No"),
+	Question("Degree", "Degree Required", "If there is a hard requirement for a degree mentioned in the job description, eg, if you MUST have a degree and there is no mention of 'or equiv experience'.", "Yes/No"),
+	Question("WebDev", "Web development Mentioned", "If the job mentions anything to do with web development.", "Yes/No"),
+	Question("PLC", "PLCs mentioned", "If the job mentions anything to do with plc's", "Yes/No"),
+	Question("Unity3D", "Unity3D mentioned", "Does the job mention Unity3D or game development in any capacity", "Yes/No"),
+	Question("Robotics", "Robotics mentioned", "Does the job mention robotics in any capacity", "Yes/No"),
+	Question("EmbeddedSystems", "Embedded Systems mentioned", "Does the job mention embedded systems in any capacity", "Yes/No"),
+	Question("MachineLearning", "Machine Learning mentioned", "Does the job mention machine learning in any capacity", "Yes/No"),
+	Question("Entrepreneurial", "Entrepreneurial Mindset", "Does the job mention any sort of need or desire for a entrepreneurial or cross disciplinary mind set", "Yes/No"),
+	Question("SmallCompany", "Small Company/Startup", "Is there any mention of this being for a small company or startup", "Yes/No"),
+	Question("WorkSummary", "Work Summary", "A brief summary of the kind of work this job might entail", "Summary"),
+	Question("SkillsRequired", "Skills Required", "A list of the skills required for this job, in order of importance.", "List of skills, comma separated")
+]
+
+longs = "\n".join([f"{i+1}. {q.long_description}" for i,q in enumerate(questions)])
+shorts = "\n".join([f"{q.short_description}: [{q.answer_format}]" for i,q in enumerate(questions)])
+system_message = Message(
+		f"""You are an automated job description quality checker. It is very important that you respond in a certain format so that your response can be parsed automatically to sort job descriptions programmatically. You will be given a copy of a job description, you must then look in the job description for several key things. Look for the following things:
+
+{longs}
 
 respond with the following format:
-Education Preference: [Yes/No]
-Degree Required: [Yes/No]
-Skills Not Listed: [List of skills]
-Experience Match: [1-10]
-Experience Utilization: [1-10]
-Job Enjoyment: [1-10]
-
-Here is an example conversation for you to go by, please respond as such:
-User:
-Here is a copy of my resume and the job description.
-
-Assistant:
-Education Preference: Yes
-Degree Required: No
-Skills Not Listed: [Java]
-Experience Match: 8
-Experience Utilization: 3
-Job Enjoyment: 7""",
+{shorts}""",
 	Role.System, source=UserSource("System")
 )
+
+print(system_message.content)
 
 def new_conversation():
 	bot.conversation = Conversation()
@@ -52,12 +55,24 @@ def new_conversation():
 #load resume:
 resume = open("resume.txt", "r").read()
 
-from JobDatabase import JobDatabase, Job
 
+
+i = 0
+JobSearchFilter.add_dynamic_columns(questions)
 db = JobDatabase("sqlite:///Job_Data/jobs.db")
-for job in db.get_all_jobs()[10:]:
+done_jobs = set([j.job_link for j in db.get_jobs_with_conversations()])
+
+for job in db.get_all_jobs():
+	if job.link in done_jobs:
+		continue
 	new_conversation()
-	job_info = bot.prompt(f"Heres a copy of my resume:\n{resume}\n========\n========\nAnd here is the job description:\n{job.long_description}")
-	print(job_info)
+	job_info = bot.prompt(f"Heres a job description:\n{job.long_description}")
 	
-	input("Press enter to continue...")
+	thing = db.insert_responses(bot.conversation.hash, job.link, questions, job_info)
+	i += 1
+	print(f"================{i}")
+	print(job.title)
+	print(job.company_name)
+	print(job.link)
+	print(job_info)
+	print("================")
